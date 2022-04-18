9818fdf63daa84be9026f1f400b57351a66812a5
ddosk <103966197+ddosk@users.noreply.github.com>
2022-04-18 23:13:51 +0200

    Add files via upload

---
+++ úkol_DD.ps1
@@ -0,0 +1,16 @@
+﻿$page = Invoke-WebRequest -Uri "https://www.cts-tradeit.cz/kariera/"
+$pozice = $page.ParsedHtml.getElementsByTagName("div") | Where-Object {$_.className -eq "row row-cols-1 row-cols-md-2 row-cols-xl-2 card-links row-xl mt-lg-4 mb-3 mb-lg-4"}
+
+$titles = ($pozice.getElementsByTagName('h3') | Where-Object { $_.className -eq 'card-title mb-0' }).innerText
+$titles | Out-File titles.txt -Force
+
+$links = ($pozice.getElementsByTagName('a') | Where-Object { $_.className -eq 'card card-lg card-link-bottom' }).ie8_href.replace("about:/","https://www.cts-tradeit.cz/")
+$links | Out-File link.txt -Force
+
+$titles
+Write-Host "`n"
+
+$links
+Write-Host "`n"
+
+Read-Host -Prompt "Press Enter to exit"
